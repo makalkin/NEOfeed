6954a7dd5a90449b1de8f8772714ce074bc5715f
Maks Ridush <makalkon@bigmir.net>
2017-10-01 19:27:01 +0300

    routing

--- src/containers/Dashboard/routes.js
+++ src/containers/Dashboard/routes.js
@@ -1,4 +1,6 @@
 import Home from '../Home';
 import { RouterGenerator } from '../../services';
 
-export const routes = new RouterGenerator('/').addRoute('/', Home, true).getRoutes();
+export const routes = new RouterGenerator(process.env.BASENAME)
+  .addRoute('/', Home, true)
+  .getRoutes();
